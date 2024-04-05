0b296af36d512c9e2f08443a8d0e4e5e706a06c6
hrantgartley <doyouevenjump@gmail.com>
2024-04-05 11:56:38 -0500

    more code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// doc ready with arrow function
-$(() => {
-    // jQuery code goes here
-    console.log("jQuery is working!")
+$(function () {
+    "use strict"
+    $(".requires-validation").each(function () {
+        $(this).on("submit", function (event) {
+            if (!this.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            } else {
+                $("#submit-message").text("Form submitted")
+            }
+
+            $(this).addClass("was-validated")
+        })
+    })
 })
